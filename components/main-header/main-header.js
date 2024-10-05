e83d4245c7b253264a2c0058440ba6ce916cdc7a
msuhyeon <job.maengsh@gmail.com>
2024-10-05 23:58:29 +0900

    Feat: Nav 메뉴만 클라이언트 컴포넌트로 분리, 활성화된 페이지에 스타일 추가

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -4,6 +4,7 @@ import Image from "next/image";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
   return (
@@ -17,10 +18,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Foodies community</Link>
+              <NavLink href="/community"> Foodies community</NavLink>
             </li>
           </ul>
         </nav>
