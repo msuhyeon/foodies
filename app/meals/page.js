8d48de2a68d1177451852100fc952146e5adebd6
msuhyeon <job.maengsh@gmail.com>
2024-10-07 23:54:08 +0900

    Feat: db에서 가져온 데이터 화면에 렌더링

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,11 @@
 import Link from "next/link";
 import MealsGrid from "@/components/meals/meals-grid";
 import classes from "./page.module.css";
+import { getMeals } from "@/lib/meals";
 
-export default function Meals() {
+export default async function Meals() {
+  const meals = await getMeals();
+
   return (
     <>
       <header className={classes.header}>
@@ -18,7 +21,7 @@ export default function Meals() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
